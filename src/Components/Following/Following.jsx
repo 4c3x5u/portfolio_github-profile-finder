d6b554eccf5b0efa75ac974344dedfc4f4f43a8f
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-06 10:24:46 +0000

    Header – Following

--- src/Components/Following/Following.jsx
+++ src/Components/Following/Following.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import followingAPI from '../../API/followingAPI';
 import FollowingSuccess from './FollowingSuccess';
-// import FollowingFailure from './FollowingFailure';
+import FollowingFailure from './FollowingFailure';
 
 const Following = () => {
   const { login } = useParams();
@@ -10,9 +10,21 @@ const Following = () => {
   const [followingFound, setFollowingFound] = useState(false);
   useEffect(() => followingAPI.get(login, setFollowing, setFollowingFound), []);
   return (
-    followingFound
-      ? <FollowingSuccess following={following} />
-      : <h1>Following Not Found</h1> // TODO: Replace this with <FollowingFailure />
+    <div className="Following">
+      <div
+        className="FollowingHead bg-dark text-center mb-2"
+        style={{
+          border: '2px solid #343a40',
+          padding: '1.2rem 0 0.7rem 0',
+          borderRadius: '2rem 2rem 0 0',
+        }}
+      >
+        <h5 className="text-light">Following</h5>
+      </div>
+      {followingFound
+        ? <FollowingSuccess following={following} />
+        : <FollowingFailure />}
+    </div>
   );
 };
 
---
+++ src/Components/Following/FollowingFailure.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const FollowingFailure = () => (<h1>Following not found.</h1>);
+
+export default FollowingFailure;
