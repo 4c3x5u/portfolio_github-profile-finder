1ab323deb5f4c1aa7d8bac0898e1d2d7708ef292
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-06 04:23:16 +0000

    Gists – GistsFailure.jsx

--- src/Components/Gists/Gists.jsx
+++ src/Components/Gists/Gists.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import gistsAPI from '../../API/gistsAPI';
 import GistsSuccess from './GistsSuccess';
-// import GistsFailure from './GistsFailure';
+import GistsFailure from './GistsFailure';
 
 const Gists = () => {
   const { login } = useParams();
@@ -12,7 +12,7 @@ const Gists = () => {
   return (
     gistsFound
       ? <GistsSuccess gists={gists} />
-      : <h1>Gists not found.</h1> // TODO: Replace this with <GistsFailure />
+      : <GistsFailure />
   );
 };
 
---
+++ src/Components/Gists/GistsFailure.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GistsFailure = () => (<h1>Gists not found.</h1>);
+
+export default GistsFailure;
