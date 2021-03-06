ea1ae1744ccc5f52c5208beebec8ed95bd8e5500
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-06 12:20:21 +0000

    Spinner – loading following

--- src/API/FollowingAPI.js
+++ src/API/FollowingAPI.js
@@ -2,23 +2,30 @@ import axios from 'axios';
 
 const parseFollowing = (following) => ({ login: following.login });
 
-const handleFollowingFound = (setFollowing, setFollowingFound) => (followingResponse) => {
-  const following = followingResponse.data.map(parseFollowing);
-  setFollowing(following);
+const handleFollowingFound = (
+  setLoading,
+  setFollowingFound,
+  setFollowing,
+) => (
+  followingResponse,
+) => {
+  setFollowing(followingResponse.data.map(parseFollowing));
   setFollowingFound(true);
+  setTimeout(() => setLoading(false), 700);
 };
 
-const handleFollowingNotFound = (setFollowing, setFollowingFound) => () => {
+const handleFollowingNotFound = (setLoading, setFollowingFound, setFollowing) => () => {
   setFollowing([]);
   setFollowingFound(false);
+  setTimeout(() => setLoading(false), 700);
 };
 
 const followingAPI = {
-  get: (login, setFollowing, setFollowingFound) => (
+  get: (login, setLoading, setFollowingFound, setFollowing) => (
     axios
       .get(`https://api.github.com/users/${login}/following`)
-      .then(handleFollowingFound(setFollowing, setFollowingFound))
-      .catch(handleFollowingNotFound(setFollowing, setFollowingFound))
+      .then(handleFollowingFound(setLoading, setFollowingFound, setFollowing))
+      .catch(handleFollowingNotFound(setLoading, setFollowingFound, setFollowing))
   ),
 };
 
--- src/Components/Following/Following.jsx
+++ src/Components/Following/Following.jsx
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import followingAPI from '../../API/followingAPI';
+import Spinner from '../Shared/Spinner/Spinner';
 import FollowingSuccess from './FollowingSuccess';
 import FollowingFailure from './FollowingFailure';
 import SubpageHeader from '../Shared/SubpageHeader';
 
 const Following = () => {
   const { login } = useParams();
-  const [following, setFollowing] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [followingFound, setFollowingFound] = useState(false);
-  useEffect(() => followingAPI.get(login, setFollowing, setFollowingFound), []);
+  const [following, setFollowing] = useState([]);
+
+  useEffect(
+    () => followingAPI.get(login, setLoading, setFollowingFound, setFollowing),
+    [],
+  );
+
+  const followingHeader = () => (<SubpageHeader subpage="Following" />);
+
+  const followingContent = () => {
+    if (loading) { return (<Spinner />); }
+    if (followingFound) { return (<FollowingSuccess following={following} />); }
+    return (<FollowingFailure />);
+  };
+
   return (
     <div className="Following">
-      <SubpageHeader subpage="Following" />
-      {followingFound
-        ? <FollowingSuccess following={following} />
-        : <FollowingFailure />}
+      {followingHeader()}
+      {followingContent()}
     </div>
   );
 };
