1a8232ecc3cac4346bffa4692acadb7383467d18
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-07 05:37:01 +0000

    Section – used in Following

--- src/Components/Following/Following.jsx
+++ src/Components/Following/Following.jsx
@@ -1,36 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import followingAPI from '../../API/followingAPI';
-import Subheader from '../Shared/Subheader/Subheader';
-import Searching from '../Shared/Searching/Searching';
+import Section from '../Shared/Section/Section';
 import FollowingFound from './FollowingFound';
 import FollowingNotFound from './FollowingNotFound';
 
-const Following = () => {
-  const { login } = useParams();
-  const [searching, setSearching] = useState(true);
-  const [found, setFound] = useState(false);
-  const [following, setFollowing] = useState([]);
-
-  useEffect(
-    () => followingAPI.search(login, setSearching, setFound, setFollowing),
-    [],
-  );
-
-  const followingHeader = () => (<Subheader title="Following" />);
-
-  const followingContent = () => {
-    if (searching) { return (<Searching />); }
-    if (found) { return (<FollowingFound following={following} />); }
-    return (<FollowingNotFound />);
-  };
-
-  return (
-    <div className="Following">
-      {followingHeader()}
-      {followingContent()}
-    </div>
-  );
-};
+const Following = () => (
+  <Section
+    title="Following"
+    api={followingAPI}
+    renderFound={(following) => <FollowingFound following={following} />}
+    renderNotFound={() => <FollowingNotFound />}
+  />
+);
 
 export default Following;
