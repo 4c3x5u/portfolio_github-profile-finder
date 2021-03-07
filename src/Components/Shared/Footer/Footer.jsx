538036dbc6ebccfe6126478adcf1a367c614f7cd
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-07 12:43:21 +0000

    Footer link

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './Footer.sass';
 
 const Footer = () => (
-  <div className="Footer bg-dark text-center">
-    <h5 className="text-light">
-      @4c3x5u/portfolio_github-profile-finder
-    </h5>
-  </div>
+  <a
+    href="https://github.com/4c3x5u/portfolio_github-profile-finder"
+    target="_blank"
+    rel="noreferrer"
+  >
+    <div className="Footer bg-dark text-center">
+      <h5 className="text-light">
+        @4c3x5u/portfolio_github-profile-finder
+      </h5>
+    </div>
+  </a>
 );
 
 export default Footer;
