c2e41b81265e7e9bfba6047c253decf5e13ff015
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-07 05:39:05 +0000

    Section – used in Repos

--- src/Components/Repos/Repos.jsx
+++ src/Components/Repos/Repos.jsx
@@ -1,36 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import reposAPI from '../../API/reposAPI';
-import Subheader from '../Shared/Subheader/Subheader';
-import Searching from '../Shared/Searching/Searching';
+import Section from '../Shared/Section/Section';
 import ReposFound from './ReposFound';
 import ReposNotFound from './ReposNotFound';
 
-const Repos = () => {
-  const { login } = useParams();
-  const [searching, setSearching] = useState(true);
-  const [found, setFound] = useState(false);
-  const [repos, setRepos] = useState([]);
-
-  useEffect(
-    () => reposAPI.search(login, setSearching, setFound, setRepos),
-    [],
-  );
-
-  const reposHeader = () => (<Subheader title="Repos" />);
-
-  const reposContent = () => {
-    if (searching) { return <Searching />; }
-    if (found) { return <ReposFound repos={repos} />; }
-    return <ReposNotFound />;
-  };
-
-  return (
-    <div className="Repos">
-      {reposHeader()}
-      {reposContent()}
-    </div>
-  );
-};
+const Repos = () => (
+  <Section
+    title="Repos"
+    api={reposAPI}
+    renderFound={(repos) => <ReposFound repos={repos} />}
+    renderNotFound={() => <ReposNotFound />}
+  />
+);
 
 export default Repos;
