a4dfc3bc4f7730f72fbfc416e07f2257c1624f1a
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-08 05:40:24 +0000

    Reusable List – Gists

--- src/Components/Gists/Gists.jsx
+++ src/Components/Gists/Gists.jsx
@@ -4,17 +4,14 @@ import Section from '../Shared/Section';
 import gistsAPI from '../../API/gistsAPI';
 import GistsFound from './GistsFound';
 
-const Gists = () => {
-  const { login } = useParams();
-  return (
-    <Section
-      title="Gists"
-      href={`https://gist.github.com/${login}`}
-      api={gistsAPI}
-      renderFound={(gists) => <GistsFound gists={gists} />}
-      hasFooter
-    />
-  );
-};
+const Gists = () => (
+  <Section
+    title="Gists"
+    href={`https://gist.github.com/${useParams().login}`}
+    api={gistsAPI}
+    renderFound={(gists) => <GistsFound gists={gists} />}
+    hasFooter
+  />
+);
 
 export default Gists;
