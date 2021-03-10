049507dd619c12b82e6b42efaef66fbb85a17d69
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-10 13:08:25 +0000

    Basic GitHub Authentication
    An environment variable added to the `.env.local` file at the root folder for the personal access token that's needed for a basic authentication with the GitHub API. The app now has a much higher request rate limit due to the use of an Authorisation header for the GET request that contains this token.

--- src/API/shared/get.js
+++ src/API/shared/get.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const handleFound = (
-  setSearching, setFound, setItems, parseItem, single,
-) => (
-  response,
-) => {
+const pta = process.env.REACT_APP_PERSONAL_ACCESS_TOKEN;
+
+const handleFound = (setSearching, setFound, setItems, parseItem, single) => (response) => {
   setItems(single ? parseItem(response.data) : response.data.map(parseItem));
   setFound(true);
   setTimeout(() => setSearching(false), 700);
@@ -18,7 +16,7 @@ const handleNotFound = (setSearching, setFound, setItems) => () => {
 
 const get = (url, setSearching, setFound, setItems, parseItem, single) => (
   axios
-    .get(url)
+    .get(url, { headers: { Authorization: `token ${pta}` } })
     .then(handleFound(setSearching, setFound, setItems, parseItem, single))
     .catch(handleNotFound(setSearching, setFound, setItems))
 );
