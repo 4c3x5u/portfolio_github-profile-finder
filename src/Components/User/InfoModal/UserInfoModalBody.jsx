436270502ea33c02820df889948cc547be509dd8
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-16 10:00:33 +0000

    InfoModal – Changes to content

--- src/Components/User/InfoModal/UserInfoModalBody.jsx
+++ src/Components/User/InfoModal/UserInfoModalBody.jsx
@@ -2,29 +2,16 @@ import React from 'react';
 
 const UserInfoModalBody = () => (
   <>
-    <p className="mb-0">
+    <p className="mb-3">
       Click on
       <em>{' Repos, Gists, Followers, Following, '}</em>
       or
       <em>{' the location '}</em>
       to toggle their corresponding sections.
     </p>
-    <br />
     <p className="mb-0">
-      You can also click on the section headers and the app footer to trigger their actions.
-    </p>
-    <br />
-    <p className="mb-0">
-      The profile section header navigates to the user&apos;s GitHub profile.
-    </p>
-    <br />
-    <p className="mb-0">
-      The other section headers navigate to either Repos, Gists, Followers, or the Following page
-      for the user on GitHub depending on its title.
-    </p>
-    <br />
-    <p className="mb-0">
-      The footer navigates to this project&apos;s GitHub page.
+      Once a section loads, you can click on list items and back buttons
+      where applicable to navigate back and forth through the app content.
     </p>
   </>
 );
