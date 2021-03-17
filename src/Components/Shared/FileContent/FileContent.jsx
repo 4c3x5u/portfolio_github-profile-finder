74332002c4cae71bb6f796042fefd83aafe4a19d
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-17 17:44:21 +0000

    FileContent – Syntax highlighter style changes

--- src/Components/Shared/FileContent/FileContent.jsx
+++ src/Components/Shared/FileContent/FileContent.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import a11yLight from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-light';
+import a11yLight from 'react-syntax-highlighter/dist/esm/styles/hljs/github';
 import './FileContent.sass';
 
 const FileContent = ({ file }) => {
   const { pathname } = useLocation();
   if (file.isImage) {
     return (
-      <div className="FileContent bg-light my-2">
+      <div className="FileContent Image bg-light my-2">
         <img
           alt="file content"
           src={
@@ -24,7 +24,7 @@ const FileContent = ({ file }) => {
     );
   }
   return (
-    <SyntaxHighlighter className="FileContent bg-light" style={a11yLight}>
+    <SyntaxHighlighter className="FileContent px-2 mb-2" style={a11yLight}>
       {file.content}
     </SyntaxHighlighter>
   );
