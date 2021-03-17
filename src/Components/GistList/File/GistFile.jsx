be52703999b98b6adb6d986ef9099c07e308e66b
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-17 10:48:32 +0000

    GistFile – Back button removed

--- src/Components/GistList/File/GistFile.jsx
+++ src/Components/GistList/File/GistFile.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import FileContent from '../../Shared/FileContent';
 import Section from '../../Shared/Section/Section';
 import { getGistFile } from '../../../API/gistAPI';
 
 const GistFile = () => {
   const { id, name } = useParams();
-  const { pathname } = useLocation();
   return (
     <Section
       title={`File: ${name}`}
       get={getGistFile(id, name)}
       view={(file) => <FileContent file={file} />}
-      backUrl={pathname.substring(0, pathname.lastIndexOf('/'))}
     />
   );
 };
