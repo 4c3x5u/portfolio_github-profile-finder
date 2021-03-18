ded5efc4c8a185ad44b8bc03d063a027c3cff59e
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-18 13:56:00 +0000

    Location – Calls the Section component

--- src/Components/Location/Location.jsx
+++ src/Components/Location/Location.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Section from '../Shared/Section/Section';
 import LocationContent from './Content/LocationContent';
+import { getLocation } from '../../API/locationAPI';
 
-const Location = () => <LocationContent />;
+const Location = () => {
+  const { address } = useLocation();
+  return (
+    <Section
+      title={`Location: ${address}`}
+      get={getLocation(address)}
+      view={(latLng) => <LocationContent latLng={latLng} />}
+    />
+  );
+};
 
 export default Location;
